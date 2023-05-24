aec1edb092b7582825f49830028b59160abf8c99
luna9528 <107362102+luna9528@users.noreply.github.com>
2023-05-23 21:35:17 -0500

    f

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+        document.querySelector(".nav-links").
+        classList.toggle("nav-links-responsive")
+    })
+
+
+function showAlert() { alert('Hola esta es una alerta'); }
